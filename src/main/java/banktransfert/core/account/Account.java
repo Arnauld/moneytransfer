532456f69ed1182c75f36515e2cd3cca0743de18
Arnauld <arnauld.loyer@gmail.com>
2019-10-09 19:08:28 +0200

    starts implementing InMemoryAccounts with concurrency in mind

--- src/main/java/banktransfert/core/account/Account.java
+++ src/main/java/banktransfert/core/account/Account.java
@@ -1,12 +1,14 @@
 package banktransfert.core.account;
 
+import banktransfert.core.Email;
+
 /**
  * @author <a href="http://twitter.com/aloyer">@aloyer</a>
  */
 public class Account {
     private final AccountId accountId;
 
-    public Account(AccountId accountId) {
+    public Account(AccountId accountId, Email email, String fullName) {
         this.accountId = accountId;
     }
 
